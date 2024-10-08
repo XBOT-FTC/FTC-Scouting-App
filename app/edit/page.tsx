49b3cdd8dafcfa72e5b9654c7db03f2f785dded6
NormalDuck <1juhuang2@duck.com>
2024-10-08 07:54:47 -0700

    remove drafting feature

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,31 +1,27 @@
 "use client";
 import { Button, ButtonGroup } from "flowbite-react";
-import { useAtom, useAtomValue } from "jotai";
 import { useState } from "react";
 
 import { NumberInput } from "@/components/number-input";
-import { draftAtom, DraftData } from "@/store/drafts";
-import { editorAtom } from "@/store/editor";
-import { DraftDataScehema } from "@/utils/DraftDataSchema";
 
 export default function Home() {
-  const [mode, setMode] = useState<"auto" | "teleop" | "end">("auto");
-  const [drafts, setDrafts] = useAtom(draftAtom);
-  const editor = useAtomValue(editorAtom);
+  const [, setMode] = useState<"auto" | "teleop" | "end">("auto");
+  // const [drafts, setDrafts] = useAtom(draftAtom);
+  // const editor = useAtomValue(editorAtom);
 
-  function newSchema() {
-    return DraftDataScehema(
-      drafts[editor].name,
-      drafts[editor].team,
-      drafts[editor].color,
-    );
-  }
+  // function newSchema() {
+  //   return DraftDataScehema(
+  //     drafts[editor].name,
+  //     drafts[editor].team,
+  //     drafts[editor].color,
+  //   );
+  // }
 
-  function updateDraft(draftSchema: DraftData) {
-    const newDrafts = [...drafts];
-    newDrafts[editor] = draftSchema;
-    setDrafts(newDrafts);
-  }
+  // function updateDraft(draftSchema: DraftData) {
+  //   const newDrafts = [...drafts];
+  //   newDrafts[editor] = draftSchema;
+  //   setDrafts(newDrafts);
+  // }
 
   return (
     <>
@@ -59,22 +55,22 @@ export default function Home() {
         <text className="flex h-10 justify-center">Scored Sample</text>
         <div className="grid grid-flow-col grid-rows-2 justify-center gap-5 text-center">
           <NumberInput
-            defaultValue={drafts[editor].auto.net}
-            onChange={(val) => {
-              const schema = newSchema();
-              if (mode === "auto") {
-                schema.auto.net = val;
-                updateDraft(schema);
-              }
-              if (mode === "teleop") {
-                schema.teleop.net = val;
-                updateDraft(schema);
-              }
-              if (mode === "end") {
-                schema.end.net = val;
-                updateDraft(schema);
-              }
-            }}
+          // defaultValue={drafts[editor].auto.net}
+          // onChange={(val) => {
+          //   const schema = newSchema();
+          //   if (mode === "auto") {
+          //     schema.auto.net = val;
+          //     updateDraft(schema);
+          //   }
+          //   if (mode === "teleop") {
+          //     schema.teleop.net = val;
+          //     updateDraft(schema);
+          //   }
+          //   if (mode === "end") {
+          //     schema.end.net = val;
+          //     updateDraft(schema);
+          //   }
+          // }}
           />
           <text>Net</text>
           <NumberInput onChange={() => {}} />
