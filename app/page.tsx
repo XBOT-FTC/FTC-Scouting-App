109bad8d146a136bce5732a16673ee6b79eb64fb
sw990 <sw7859305@gmail.com>
2024-10-11 15:24:15 -0700

    changed layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid justify-center">
-      <Image src="/Image.png" width={201} height={191} alt="Picture of logo" />
-      <div className="grid justify-center text-2xl">
-        <p>
-          <b>Welcome!</b>
-        </p>
+    <div className="grid justify-center bg-gradient-to-b from-teal-400 from-20% via-teal-700 via-30% to-teal-900 to-85% text-white">
+      <div className="grid justify-center bg-transparent">
+        <Image
+          src="images/image.png"
+          width={201}
+          height={191}
+          alt="Picture of logo"
+        />
       </div>
-      <div className="h-428 w-277 box-content border-black p-4"></div>
-      <form action={"/url"} method="GET">
-        <p>Enter Name</p>
-        <input type="text" placeholder="Name" required></input>
-        <div className="grid justify-center">
-          <button type="submit">Next</button>
+      <div className="box-content border-black bg-white p-10">
+        <div className="mb-10 grid justify-center text-2xl text-black">
+          <p>
+            <b>Welcome</b>
+          </p>
         </div>
-      </form>
-      <div className="grid justify-center">XBOT ROBOTICS</div>
+
+        <div className="grid justify-center border-black bg-white text-black">
+          <form action={"/url"} method="GET">
+            <div className="text-left text-black">
+              <p>
+                <b>Enter Name</b>
+              </p>
+            </div>
+            <input type="text" placeholder="Name" required></input>
+            <div className="m-20 grid justify-center rounded-lg bg-blue-400 box-decoration-slice p-3 text-white">
+              <button type="submit">Next</button>
+            </div>
+          </form>
+        </div>
+
+        <div className="mt-10 grid justify-center text-gray-400">
+          <p>XBOT ROBOTICS</p>
+        </div>
+      </div>
     </div>
   );
 }
