236314ac85023ab7f4ae8abc91ff9323b235e6bd
sw990 <sw7859305@gmail.com>
2024-10-23 19:25:22 -0700

    pulling data from frc-scout

---
+++ backend/query.js
@@ -0,0 +1,27 @@
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    gql
+  } from "@apollo/client";
+
+  const client = new ApolloClient({
+    uri: 'https://api.ftcscout.org/graphql',
+    cache: new InMemoryCache()
+  });
+
+  client.query({
+    query: gql`
+    query {
+  eventByCode(code: "USWAHALT", season: 2023) {
+    matches {
+      matchNum
+      teams {
+        alliance
+        teamNumber
+      }
+    }
+  }
+}
+    `,
+  }).then((result)=>console.log(JSON.stringify(result)));
