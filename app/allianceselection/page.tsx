fbff43bf18e9a0eb0f1ba83bb5022d0e0b09637f
sw990 <sw7859305@gmail.com>
2024-11-20 19:34:23 -0800

    working drop down menu

--- app/allianceselection/page.tsx
+++ app/allianceselection/page.tsx
@@ -1,17 +1,29 @@
 "use client";
-import { Modal } from "flowbite-react";
+import { MatchCollection } from "@/types/team-properties";
 import { useState } from "react";
-
-fetch("/api/fetch-matches", {
-  method: "POST",
-  body: JSON.stringify({ collection: "League Meet 1", matches: [] }),
-}).then(async (value) => {
-  if (value.ok) {
-    console.log(value);
-  }
-});
+import { useEffectOnce } from "react-use";
 
 export default function Home() {
+  const [response, setResponse] = useState<MatchCollection>();
+
+  useEffectOnce(() => {
+    fetch("/api/fetch-matches", {
+      method: "POST",
+      body: JSON.stringify({ collection: "League Meet 1", matches: [] }),
+    }).then(async (value) => {
+      const matchData: MatchCollection = await value.json();
+      setResponse(matchData);
+      alert("match data:" + matchData.length);
+      // const selectElement = document.getElementById("matchNumber");
+      // for (let i = 1; i < matchData.length + 1; i++) {
+      //   const option = document.createElement("option");
+      //   option.value = i.toString();
+      //   option.text = i.toString();
+      //   selectElement?.appendChild(option);
+      // }
+    });
+  });
+
   return (
     <div className="grid justify-center bg-gradient-to-b from-teal-400 from-20% via-teal-700 via-30% to-teal-900 to-85% text-white">
       <form>
@@ -21,8 +33,10 @@ export default function Home() {
           name="matchNumber"
           id="matchNumber"
         >
-          <option>Match Number</option>
-          <option>1</option>
+          <option disabled>Match Number</option>
+          {response?.map((matchNum) => {
+            return <option>{matchNum.match}</option>;
+          })}
         </select>
         <br></br>
       </form>
