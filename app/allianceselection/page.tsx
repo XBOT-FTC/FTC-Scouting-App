ff5eb8fd05b9c45a6fcce3f364d2983e05dd2b93
sw990 <sw7859305@gmail.com>
2024-11-20 18:07:20 -0800

    restyled drop down menu

--- app/allianceselection/page.tsx
+++ app/allianceselection/page.tsx
@@ -2,29 +2,30 @@
 import { Modal } from "flowbite-react";
 import { useState } from "react";
 
-const items: any[] = [];
-const dropdown = document.getElementById("matches");
 fetch("/api/fetch-matches", {
   method: "POST",
   body: JSON.stringify({ collection: "League Meet 1", matches: [] }),
-}).then(async (value) => {});
-
-items.forEach((item) => {
-  const option = document.createElement("option");
-  option.value = item.replace("", "");
-  option.textContent = item;
-  dropdown?.appendChild(option);
+}).then(async (value) => {
+  if (value.ok) {
+    console.log(value);
+  }
 });
 
 export default function Home() {
   return (
     <div className="grid justify-center bg-gradient-to-b from-teal-400 from-20% via-teal-700 via-30% to-teal-900 to-85% text-white">
       <form>
-        <label htmlFor="matchNumber">Match #:</label>
-        <select name="matchNumber" id="matchNumber"></select>
+        <label htmlFor="matchNumber"></label>
+        <select
+          className="rounded-md text-black"
+          name="matchNumber"
+          id="matchNumber"
+        >
+          <option>Match Number</option>
+          <option>1</option>
+        </select>
         <br></br>
       </form>
-      <div className=""></div>
     </div>
   );
 }
