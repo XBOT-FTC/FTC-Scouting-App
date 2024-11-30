d8b0d190ad7bb93a17b305ceb5b9bf185f161c2d
junjie <1juhuang2@duck.com>
2024-11-29 21:34:48 -0800

    fix issue with continue button being disabled

--- app/allianceselection/page.tsx
+++ app/allianceselection/page.tsx
@@ -2,7 +2,7 @@
 import { Button } from "flowbite-react";
 import { produce } from "immer";
 import { useAtom } from "jotai";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useEffectOnce } from "react-use";
 
@@ -14,8 +14,10 @@ import { MatchCollection } from "@/types/team-properties";
 export default function Home() {
   const [response, setResponse] = useState<MatchCollection>();
   const [matchNumber, setMatchNumber] = useAtom(matchAtom);
+  const [team, setTeam] = useState<number>(0);
   const [cursor, setCursor] = useState<number>(0);
   const [localDraft, setLocalDraft] = useAtom(localDraftAtom);
+  const router = useRouter();
 
   useEffectOnce(() => {
     fetch("/api/fetch-matches", {
@@ -29,94 +31,97 @@ export default function Home() {
 
   return (
     <div className="grid justify-center bg-gradient-to-b from-teal-400 from-20% via-teal-700 via-30% to-teal-900 to-85% text-white">
-      <form>
-        <select
-          className="rounded-md text-black"
-          name="matchNumber"
-          id="matchNumber"
-          defaultValue="Match Number"
-          onChange={(event) => {
-            setMatchNumber(Number(event.currentTarget.value));
-            const match = response!.find(
-              (value) => value.match === Number(event.currentTarget.value),
-            )!;
-            setLocalDraft(
-              produce(localDraft, (draft) => {
-                draft.name = match.teams[cursor].name;
-                draft.team = match.teams[cursor].team;
-                draft.color = match.teams[cursor].color;
-              }),
-            );
-          }}
-          required
+      <select
+        className="rounded-md text-black"
+        name="matchNumber"
+        id="matchNumber"
+        defaultValue="Match Number"
+        onChange={(event) => {
+          setMatchNumber(Number(event.currentTarget.value));
+          const match = response!.find(
+            (value) => value.match === Number(event.currentTarget.value),
+          )!;
+          setLocalDraft(
+            produce(localDraft, (draft) => {
+              draft.name = match.teams[cursor].name;
+              draft.team = match.teams[cursor].team;
+              draft.color = match.teams[cursor].color;
+            }),
+          );
+        }}
+      >
+        <option className="text-gray-500" disabled>
+          Match Number
+        </option>
+        {!(response === undefined) ? (
+          response.map((match) => {
+            return <option key={match.match}>{match.match}</option>;
+          })
+        ) : (
+          <></>
+        )}
+      </select>
+      <div className="mb-5" />
+      <select
+        className="rounded-md text-black"
+        name="matchNumber"
+        id="matchNumber"
+        defaultValue="Team Number"
+        onChange={(event) => {
+          setTeam(Number(event.currentTarget.value));
+          const match = response!.find((value) => value.match === matchNumber)!;
+          setCursor(
+            match.teams.findIndex(
+              (value) => value.team === Number(event.currentTarget.value),
+            ),
+          );
+          setLocalDraft(
+            produce(localDraft, (draft) => {
+              const result = response
+                ?.find((match) => match.match === matchNumber)
+                ?.teams.find(
+                  (team) => team.team === Number(event.currentTarget.value),
+                );
+              draft.name = result!.name;
+              draft.team = result!.team;
+              draft.color = result!.color;
+            }),
+          );
+        }}
+      >
+        <option
+          key={team}
+          className="text-gray-500"
+          value={"Team Number"}
+          disabled
         >
-          <option className="text-gray-500" disabled>
-            Match Number
-          </option>
-          {!(response === undefined) ? (
-            response.map((match) => {
+          Team Number
+        </option>
+        {response ? (
+          response
+            ?.find((val) => val.match === matchNumber)
+            ?.teams.map((value, i) => {
               return (
-                <option onChange={() => {}} key={match.match}>
-                  {match.match}
+                <option disabled={value.scouted} key={i}>
+                  {value.team}
                 </option>
               );
             })
-          ) : (
-            <></>
-          )}
-        </select>
-        <div className="mb-5" />
-        <select
-          className="rounded-md text-black"
-          name="matchNumber"
-          id="matchNumber"
-          defaultValue="Team Number"
-          required
-        >
-          <option
-            key={cursor}
-            className="text-gray-500"
-            value={"Team Number"}
-            disabled
-          >
-            Team Number
-          </option>
-          {response ? (
-            response
-              ?.find((val) => val.match === matchNumber)
-              ?.teams.map((value, i) => {
-                return (
-                  <option
-                    disabled={value.scouted}
-                    onClick={() => {
-                      setCursor(i);
-                      setLocalDraft(
-                        produce(localDraft, (draft) => {
-                          draft.name = value.name;
-                          draft.team = value.team;
-                          draft.color = value.color;
-                        }),
-                      );
-                    }}
-                    key={value.team}
-                  >
-                    {value.team}
-                  </option>
-                );
-              })
-          ) : (
-            <></>
-          )}
-        </select>
-        <div className="mb-5" />
-        <Button
-          type="submit"
-          disabled={matchNumber === 0 || cursor === 0}
-          className="flex justify-center"
-        >
-          <Link href={"/auto"}> Continue</Link>
-        </Button>
-      </form>
+        ) : (
+          <></>
+        )}
+      </select>
+      <div className="mb-5" />
+      <Button
+        disabled={team === 0 || matchNumber === 0}
+        onClick={() => {
+          if (!(team === 0 || matchNumber === 0)) {
+            router.push("/auto");
+          }
+        }}
+      >
+        Continue
+      </Button>
     </div>
   );
 }
